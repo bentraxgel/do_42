8fd9b0d7907de80f1c02b72bb582244924ce5396
bentraxgel <dhrtpdnjs@gmail.com>
2023-02-21 20:31:45 +0900

    2023/02/21 - 20:31:45 backup

--- finish/get_next_line_molla.c
+++ finish/get_next_line_molla.c
@@ -6,7 +6,7 @@
 /*   By: seok <seok@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/18 03:18:32 by quesera           #+#    #+#             */
-/*   Updated: 2023/02/21 20:31:37 by seok             ###   ########.fr       */
+/*   Updated: 2023/02/21 19:00:48 by seok             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,37 +14,69 @@
 
 void	my_lst_free(t_list *find, t_list *head)
 {
+	if (find == NULL) //TODO : 필요성이 있는 예외처리인가?
+		return ;
 	t_list	*temp;
 
 	temp = head;
 	while (temp->next != NULL && temp->next != find)
 		temp = temp->next;
-	if (head == NULL)
-		free(head);
+	// while (head->next != NULL && head->next != find)
+	// 	head = head->next;
+
+	// if (head->next == NULL)
+	// {
+		if (head == NULL)
+			free(head);
+	// 	return ;
+	// }
+
 	if (find->save != NULL)
 		free(find->save);
 	temp->next = find->next;
+	// head->next = find->next;
 	find->next = NULL;
 	free(find);
+
+	// if (head->next == NULL)
+	// {
+	// 	if (head == NULL)
+	// 		free(head);
+	// 	return ;
+	// }
+
+	// if (head == NULL)
+	// 	free(head);
+	
+	// find = NULL;
 }
 
 t_list	*my_lst_find(t_list **head, int f_fd)
 {
 	t_list	*temp;
-
-	if (*head == NULL)
-	{
-		(*head) = malloc(sizeof(t_list));
-		if (!(*head))
-			return (NULL);
-		(*head)->fd = -1;
-		(*head)->next = NULL;
-	}
+//TODO : 3
+//TODO : head마ㄴ들기 gnl()에 있던거 일단 올림
+	// if (*head == NULL)
+	// {
+	// 	(*head) = malloc(sizeof(t_list));
+	// 	if (!(*head))
+	// 		return (NULL);
+	// 	(*head)->fd = -1;
+	// 	(*head)->next = NULL;
+	// }
 	temp = *head;
 	while (temp)
 	{
 		if (temp->fd == f_fd)
+		{
+			//TODO : free_1
+			// if (read(f_fd, NULL, 0) < 0)
+			// {
+			// 	my_lst_free(temp, *head);
+			// 	return (0);
+			// }
 			return (temp);
+		}
 		temp = temp->next;
 	}
 	while ((*head)->next)
@@ -69,9 +101,7 @@ char	*my_save_buf(t_list *find, t_list *head)
 	ret = 0;
 	while (ret == 0)
 	{
-		idx = BUFFER_SIZE + 1;
-		while (idx--)
-			find->buf[idx] = 0;
+		ft_memset(find->buf, 0, BUFFER_SIZE + 1);
 		check = read(find->fd, find->buf, BUFFER_SIZE);
 		find->save = ft_strjoin(find->save, find->buf);
 		idx = -1;
@@ -82,7 +112,7 @@ char	*my_save_buf(t_list *find, t_list *head)
 				ret = ft_substr(find->save, 0, idx + 1);
 				temp = ft_substr(find->save, idx + 1,\
 								ft_strlen(find->save) - (idx + 1));
-				free(find->save);
+				free(find->save); //TODO : 명준방식?
 				find->save = temp;
 				return (ret);
 			}
@@ -112,22 +142,84 @@ char	*get_next_line(int fd)
 	static t_list	*head;
 	t_list			*find;
 	char			*ret;
+//TODO : 1
+	// if (BUFFER_SIZE <= 0 || read(fd, NULL, 0) < 0 || check == 0)
+	if (head == NULL)
+	{
+		head = malloc(sizeof(t_list));
+		if (!head)
+			return (NULL);
+		head->fd = -1;
+		head->next = NULL;
+	}
+	// find = my_lst_find(&head, fd); //TODO : 4
+	/*
 
+	if(find == NULL)
+	{
+		my_lst_free(find, head);
+		return (NULL);
+	}
+	*/
+	find = head;
+	while (find && find->fd != fd)
+	{
+		find = find->next;
+	}
+	if (find == NULL)
+	{
+		if (head == NULL)
+			free(head);
+		return (NULL);
+	}
+		
+	// if (BUFFER_SIZE <= 0 || read(fd, NULL, 0) < 0 || find == NULL)
 	if (BUFFER_SIZE <= 0 || read(fd, NULL, 0) < 0)
 	{
-		find = head;
-		while (find)
-		{
-			if (find->fd == fd)
-			{
-				my_lst_free(find, head);
-				return (NULL);
-			}
-			find = find->next;
-		}
+		
+		// if (read(fd, NULL, 0) < 0)
+		// {
+		// 	find = head;
+		// 	while (find)
+		// 	{
+		// 		if (find->fd == fd)
+		// 			free(find);
+		// 			// my_lst_free(find, head);
+		// 		find = find->next;
+		// 	}
+		// }
+		// if (head && head->next == NULL)
+		// 	free(head);
+		
+		//TODO : 4 : segfault
+		// if (find != NULL)
+		// 	my_lst_free(find, head); //TODO : free_2 (leak x)
+		
+		// if (head->fd == -1 && head->next == NULL)
+		// 	free(head);
+			// my_lst_free(head, head);
+
+		// if (read(fd, NULL, 0) <= 0)
+		// {
+		// 	find = my_lst_find(&head, fd);
+		// 	my_lst_free(find, head);
+			// if (head && head->next == NULL)
+			// 	// my_lst_free(head, head);
+			// 	free(head);
+		// }
 		return (0);
 	}
+	//TODO : 2
+	// if (head == NULL)
+	// {
+	// 	head = malloc(sizeof(t_list));
+	// 	if (!head)
+	// 		return (NULL);
+	// 	head->fd = -1;
+	// 	head->next = NULL;
+	// }
 	find = my_lst_find(&head, fd);
+
 	ret = my_save_buf(find, head);
 	if (head->fd == -1 && head->next == NULL)
 	{
--- finish/main.c
+++ finish/main.c
@@ -17,29 +17,39 @@ int	main(void)
 	atexit(leak_check);
 	idx = 1;
 	// printf("1_fd : %d\n", fd);
-	str = get_next_line(-1);
-	printf("%s\n", str);
-	str = get_next_line(100);
-	printf("%s\n", str);
-	fd = open("text.txt", O_RDWR);
-	close(fd);
-	printf("fd : %d\n", fd);
-	str = get_next_line(fd);
-	printf("%s\n", str);
+
+	// str = get_next_line(-1);
+	// printf("%s\n", str);
+	// str = get_next_line(100);
+	// printf("%s\n", str);
 	// fd = open("text.txt", O_RDWR);
-	// printf("1_fd : %d\n", fd);
-	// while (1)
-	// {
-	// 	str = get_next_line(fd, 1);
-	// 	printf("%d: <%s\n", idx, str);
-	// 	idx++;
-	// 	if (!str)
-	// 	{
-	// 		free(str);
-	// 		break ;
-	// 	}
-	// 	free(str);
-	// }
+	// close(fd);
+	// printf("close_fd : %d\n", fd);
+	// str = get_next_line(fd);
+	// printf("%s\n", str);
+	fd = open("text.txt", O_RDWR);
+	printf("new open_fd : %d\n", fd);
+
+		str = get_next_line(fd, 0);
+		printf("%d: <%s\n", idx, str);
+		str = get_next_line(fd, 0);
+		printf("%d: <%s\n", idx, str);
+		str = get_next_line(fd, 1);
+		printf("%d: <%s\n", idx, str);
+		close(fd);
+		fd = open("text.txt", O_RDWR);
+	while (1)
+	{
+		str = get_next_line(fd, 0);
+		printf("%d: <%s\n", idx, str);
+		idx++;
+		if (!str)
+		{
+			free(str);
+			break ;
+		}
+		free(str);
+	}
 	close(fd);
 	return (0);
 }
