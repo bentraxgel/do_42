29301d7b2a36cbd5b0bc2172c357e4873d548dcd
bentraxgel <dhrtpdnjs@gmail.com>
2023-02-21 22:47:52 +0900

    2023/02/21 - 22:47:52 backup

---
+++ finish/42/get_next_line.h
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seok <seok@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/18 01:59:31 by seok              #+#    #+#             */
+/*   Updated: 2023/02/21 21:05:51 by seok             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
+
+# include <stdlib.h>
+# include <unistd.h>
+
+typedef struct s_list
+{
+	int				fd;
+	char			buf[BUFFER_SIZE + 1];
+	char			*save;
+	struct s_list	*next;
+}t_list;
+
+size_t	ft_strlen(const char *s);
+void	*ft_memset(void *str, int value, size_t len);
+char	*ft_substr(char *s, unsigned int start, size_t len);
+void	*ft_memcpy(void *dest, const void *src, size_t len);
+char	*ft_strjoin(char *s1, char *s2);
+t_list	*my_lst_find(t_list **head, int f_fd);
+void	my_lst_free(t_list *find, t_list *head);
+void	my_check_zero(t_list *head, t_list *find, int check, char **ret);
+// char	*my_save_buf(t_list *find, t_list *head, char *ret);
+char	*my_save_buf(int fd, t_list **head);
+// char	*get_next_line(int fd, int check); //TODO check del
+char	*get_next_line(int fd); //TODO check del
+
+#endif
