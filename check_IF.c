1cc9f44ca00543cffbbca17aeeed55a26f5f5923
bentraxgel <dhrtpdnjs@gmail.com>
2023-04-15 23:16:08 +0900

    2023/04/15 - 23:16:08 backup

--- check_IF.c
+++ check_IF.c
@@ -6,12 +6,19 @@
 /*   By: seok <seok@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/15 22:34:58 by seok              #+#    #+#             */
-/*   Updated: 2023/04/15 22:46:38 by seok             ###   ########.fr       */
+/*   Updated: 2023/04/15 23:16:06 by seok             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "printf.h"
-
+//TODO 제일 먼저 할 것!
+// format[idx]를 주기보단, var_arg(ap, type)을 하여
+// 바로 값을 주는건 어때?
+// 또는
+// 현재 문제점 : const char *fornmat이라서 strjoin을 못하고있는데,
+// char c = format[idx];해서 c를 my_c로 보내는것
+// 근데 어쩌피 서식지정자의 값이 중요한게 아니라 그로인해 받은
+//가변인자의 값이 중요한거라 위에꺼가 더 맞는 말일듯? test필요
 int	my_check_if(const char *format, int idx, char **str)
 {
 	if (format[idx] == 'c')
