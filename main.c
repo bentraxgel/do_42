861f8503c072b00d30ad3e5afd422535c840c978
bentraxgel <dhrtpdnjs@gmail.com>
2023-04-29 09:25:49 +0900

    2023/04/29 - 09:25:49 backup

--- main.c
+++ main.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.c                                        :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: seok <seok@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/26 19:33:15 by seok              #+#    #+#             */
-/*   Updated: 2023/04/27 17:25:04 by seok             ###   ########.fr       */
+/*   Updated: 2023/04/29 02:25:37 by seok             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,12 @@
 
 //	write(2, "ERROR\n", 6); //TODO 함수종료
 
-int	main(int argc, char **argv)
+int	main(int argc, char *argv[])
 {
 	int		i = 0;
 	char	*arg;
 	char	*space;
+	int		*num;
 
 	space = " ";
 	if (argc == 1)
@@ -29,6 +30,9 @@ int	main(int argc, char **argv)
 		arg = ft_strjoin_free(arg, argv[i]);
 		arg = ft_strjoin_free(arg, space);
 	}
+	// 문자 -> 숫자로 바꿔서 하나씩 담기.
+	num = my_atoi(argv)
+	my_atoi()
 	printf("arg : %s\n", arg);
 	return (0);
 }
