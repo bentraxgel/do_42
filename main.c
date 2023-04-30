acd6f85e58b9983aca3001adf0bb77117abd7128
bentraxgel <dhrtpdnjs@gmail.com>
2023-04-30 16:41:46 +0900

    2023/04/30 - 16:41:46 backup

--- main.c
+++ main.c
@@ -6,22 +6,22 @@
 /*   By: seok <seok@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/26 19:33:15 by seok              #+#    #+#             */
-/*   Updated: 2023/04/29 02:25:37 by seok             ###   ########.fr       */
+/*   Updated: 2023/04/30 16:15:57 by seok             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include <stdio.h>
 
-//	write(2, "ERROR\n", 6); //TODO 함수종료
-
 int	main(int argc, char *argv[])
 {
-	int		i = 0;
+	int		i;
 	char	*arg;
 	char	*space;
+	char	**word;
 	int		*num;
 
+	i = 0;
 	space = " ";
 	if (argc == 1)
 		return (0);
@@ -30,9 +30,14 @@ int	main(int argc, char *argv[])
 		arg = ft_strjoin_free(arg, argv[i]);
 		arg = ft_strjoin_free(arg, space);
 	}
-	// 문자 -> 숫자로 바꿔서 하나씩 담기.
-	num = my_atoi(argv)
-	my_atoi()
-	printf("arg : %s\n", arg);
+	word = ft_split(arg, ' ');
+	i = 0;
+	while (word[i])
+		i++;
+	num = (int *)malloc(sizeof(int *) * (i + 1));
+	while (--i >= 0)
+		num[i] = ft_atoi_pro(word[i]);
+	for (int i = 0; i < 5; i++)
+		printf("num[%d] : %d\n", i, num[i]);
 	return (0);
 }
