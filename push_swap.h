efe30d65637078b0b2206a10126fa3a36a5b449b
bentraxgel <dhrtpdnjs@gmail.com>
2023-05-04 15:46:25 +0900

    window_enum

--- push_swap.h
+++ push_swap.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: seok <seok@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: seok <seok@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/26 19:33:37 by seok              #+#    #+#             */
-/*   Updated: 2023/04/30 14:37:53 by seok             ###   ########.fr       */
+/*   Updated: 2023/05/04 15:17:47 by seok             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,23 @@
 
 #include "libft/libft.h"
 #include <unistd.h>
+
+typedef enum e_set
+{
+	STACK_A = 0,
+	STACK_B,
+}t_set;
+
+typedef struct s_info
+{
+	t_list		*command;
+	int			*a;
+	int			*b;
+	int			*sort;
+	long long	a_len;
+	long long	b_len;
+	long long	total_len;
+} t_info;
 
 void	my_error();
 
